44e258a0a4a231763a123291ee2bd84f09191f22
Zong Lei <leasingzong@gmail.com>
2018-06-17 10:56:42 +0800

    init ZZMainViewKit

---
+++ Example/Pods/Target Support Files/Pods-ZZMainViewKit_Example/Pods-ZZMainViewKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZZMainViewKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZZMainViewKit_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-ZZMainViewKit_Example/Pods-ZZMainViewKit_Example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ZZMainViewKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ZZMainViewKit_ExampleVersionString[];
+
